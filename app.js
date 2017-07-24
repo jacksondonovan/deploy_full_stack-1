2018b869b6b3896d512c2e043913fb4ec8945858
Jackson Donovan <iinventedtwitter@gmail.com>
2017-07-24 14:51:38 -0600

    added nothing

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var db = require('./db/knex');
 
 var app = express();
 
+
+//line
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
