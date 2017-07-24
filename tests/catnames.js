5180f2671946e29c6c1ba81b44b5ff37a45d2aaf
Jackson Donovan <iinventedtwitter@gmail.com>
2017-07-24 14:13:11 -0600

    added a test

---
+++ tests/catnames.js
@@ -0,0 +1,25 @@
+const chai = require('chai')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+const should = chai.should();
+
+describe('cat names API',function(){
+  it('should return all cats',function(done){
+    api.get('/catnames')
+    .expect(200)
+    .end((err,res)=>{
+      if(err) return done(err)
+
+      res.body[0].name.should.be.a('string')
+
+      done()
+    })
+  })
+  it('should post a cat name',function(done){
+    api.post('/catnames')
+    .send({name: 'my awesome new cat name'})
+    .expect(200,done)
+  })
+})
